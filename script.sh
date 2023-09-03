41dae4a53d5423e3991c7e88c73d6b9726daa2b5
thftgr <thftgr@gmail.com>
2023-09-03 19:15:18 +0900

    osz 제거 옵션 오류 수정

--- script.sh
+++ script.sh
@@ -1,6 +1,10 @@
 # 파생 파일 일괄 삭제
 find ./* -maxdepth 1 -type f \( -name '*nv*' -o -name '*nh*' -o -name '*ns*' -o -name '*nb*' \) -exec rm -f {} +
 
+# 파생 파일 검색
+find ./* -maxdepth 1 -type f \( -name '*nv*' -o -name '*nh*' -o -name '*ns*' -o -name '*nb*' \)
+
+# 파생 파일중 포함된것만 삭제
 find ./* -maxdepth 1 -type f -name '*nv*' -exec rm -f {} +
 find ./* -maxdepth 1 -type f -name '*nh*' -exec rm -f {} +
 find ./* -maxdepth 1 -type f -name '*ns*' -exec rm -f {} +
